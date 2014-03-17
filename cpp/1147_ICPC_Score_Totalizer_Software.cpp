1f33c6a1517a615476b5dd18c06227c56914768d
Junichi Kobayashi <nnujabok@gmail.com>
2014-03-17 16:51:00 +0900

    Add some cpp files

--- cpp/1147_ICPC_Score_Totalizer_Software.cpp
+++ cpp/1147_ICPC_Score_Totalizer_Software.cpp
@@ -1,1 +1,23 @@
-﻿#include
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	
+	while(true){
+		int n;
+		cin >> n;
+		if(n==0) break;
+		int max=0,min=1000,sum=0;
+		for(int i=0;i<n;i++){
+			int a;
+			cin >> a;
+			sum+=a;
+			if(max<a) max=a;
+			if(min>a) min = a;
+		}
+		int result = (sum-max-min) / (n-2);
+		cout << result << endl;
+	}
+	
+	return 0;
+}
