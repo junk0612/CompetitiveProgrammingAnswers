6d8cefd87afc9eecfa3bb441395e26cd356dfaf2
Junichi Kobayashi <nnujabok@gmail.com>
2014-09-27 23:03:13 +0900

    Add SRM 634 Div1 Easy

---
+++ TopCoder/SRMs/SRM634/Div1_250_ShoppingSurveyDiv1/ShoppingSurveyDiv1.java
@@ -0,0 +1,14 @@
+public class ShoppingSurveyDiv1 {
+
+	public int minValue(int N, int K, int[] s) {
+		for (int i = 0; i < N + 1; i++) {
+			int left = 0;
+			for (int j = 0; j < s.length; j++)
+				if (s[j] > i)
+					left += s[j] - i;
+			if (left <= (N - i) * (K - 1))
+				return i;
+		}
+		return N;
+	}
+}
